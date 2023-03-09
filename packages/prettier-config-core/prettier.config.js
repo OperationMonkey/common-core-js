cd8d36d51626271c47ddef84f9784a774a0ab76a
Ville Koskela <vkoskela@gmail.com>
2023-03-09 21:33:21 +0200

    chore: update prettier rules

--- packages/prettier-config-core/prettier.config.js
+++ packages/prettier-config-core/prettier.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  printWidth: 100,
+  useTabs: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always"
 }
