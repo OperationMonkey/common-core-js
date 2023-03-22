32091cdca6da18ceacfcc2f5fb9c2383159217c5
Ville Koskela <vkoskela@gmail.com>
2023-03-22 21:11:40 +0200

    feat: add jest to lint-staged

--- packages/lint-staged-config-core/lint-staged.config.js
+++ packages/lint-staged-config-core/lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   "*.md": ["prettier --write"],
   "*.{ts,tsx}": ["prettier --write"],
-  "*.{js,jsx,json,ts,tsx}": ["eslint --quiet"]
-}
+  "*.{js,jsx,json,ts,tsx}": ["eslint --quiet"],
+  "*.{js,jsx,ts,tsx}": ["jest -b --findRelatedTests --passWithNoTests"],
+};
